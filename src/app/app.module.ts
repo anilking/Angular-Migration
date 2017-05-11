bc22347d781b0e20ba7914e6b0b9d526a30bab2a
anilpathuri <anil.pathuri@walkingtree.tech>
2017-05-11 16:03:59 +0530

    title changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,27 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './pages/header/header.component';
+import { FooterComponent } from './pages/footer/footer.component';
+import { ContentComponent } from './pages/content/content.component';
+import { EmployeeService } from './service/employee.service';
+import { SearchPipe } from './pipes/search.pipe';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    ContentComponent,
+    SearchPipe
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule
   ],
-  providers: [],
+  providers: [EmployeeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
