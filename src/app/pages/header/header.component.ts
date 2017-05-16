a63a490d07e987c3604ac1d72a035f83c00dd22f
kanchansingh1123 <kanchan.singh@walkingtree.tech>
2017-05-16 17:33:09 +0530

    Modified Code for Languages File.

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -10,55 +10,43 @@ export class HeaderComponent implements OnInit {
 
   
   public supportedLangs: any;
-  public translatedText: string;
   public selectedLang: any;
 
   constructor(public _translate: TranslateService) { 
 
   }
 
-  ngOnInit() { 
-      this.selectLang(this);
+  ngOnInit() {
+      let lang : string = localStorage.getItem('lang');
+      this.selectLang(lang);
   }
 
   selectedValue(){
     this.selectLang(this.selectedLang);
   }
 
-  // isCurrentLang(lang: any) {
-  //   this.selectLang('en');
-  //   return lang === this._translate.currentLang;
-  // }
+  selectLang(lang ? : any) {
+    if(!lang) {
+      lang = 'en';
+    }
 
-  selectLang(lang: any) {
-    debugger;
+    localStorage.setItem('lang', lang);
+
     // set default;
-    if( lang == 'en'){
+    if( lang == 'en') {
           this.supportedLangs = [
             { display: 'English', value: 'en', id: '0' },
-            { display: 'Hindi', value: 'hi', id: '1' },
-            // { display: 'Chinese', value: 'zh', id: '2' }
+            { display: 'Hindi', value: 'hi', id: '1' }
           ];
           this.selectedLang = this.supportedLangs[0].value;
-      }else{
+      } else {
           this.supportedLangs = [
             { display: 'अंग्रेज़ी', value: 'en' },
-            { display: 'हिंदी', value: 'hi' },
-            // { display: 'चीनी', value: 'zh' }
+            { display: 'हिंदी', value: 'hi' }
           ];
           this.selectedLang = this.supportedLangs[1].value;
       }
      this._translate.use(lang);
-     this.refreshText();
-  }
-
-  refreshText() {
-    this.translatedText = this._translate.instant(['Angular',
-    'Employee',
-    'Name',
-    'Age',
-    'search by name',
-    'Change Language'])
   }
 
 }
--- src/app/service/translations.ts
+++ src/app/service/translations.ts
@@ -1,8 +1,5 @@
-import { LANG_ZH_NAME, LANG_ZH_TRANS } from './../pages/languages/zh';
-import { LANG_ES_NAME, LANG_ES_TRANS } from './../pages/languages/es';
 import { LANG_EN_NAME, LANG_EN_TRANS  } from './../pages/languages/en';
 import { LANG_HI_NAME, LANG_HI_TRANS } from './../pages/languages/hi';
-import { LANG_FI_NAME, LANG_FI_TRANS } from './../pages/languages/FI';
 import { OpaqueToken } from '@angular/core';
 
 
@@ -10,11 +7,8 @@ import { OpaqueToken } from '@angular/core';
 export const TRANSLATIONS = new OpaqueToken('translations');
 
 // all traslations
-export const dictionary = {
+const dictionary = {
 	[LANG_EN_NAME]: LANG_EN_TRANS,
-	[LANG_ES_NAME]: LANG_ES_TRANS,
-	[LANG_ZH_NAME]: LANG_ZH_TRANS,
-	[LANG_FI_NAME]: LANG_FI_TRANS,
 	[LANG_HI_NAME]: LANG_HI_TRANS,
 };
 
